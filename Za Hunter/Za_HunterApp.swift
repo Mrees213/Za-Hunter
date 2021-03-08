514cce142dfc62b5f92de75d2f1ef1fe53f31f87
Morgan Rees <morgan.w.rees@gmail.com>
2021-03-08 10:21:36 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Morgan w Rees on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
